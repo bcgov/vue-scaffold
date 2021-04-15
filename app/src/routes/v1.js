4d8d9553a9f5a1c71c1f8f3d81c6b808a3a8a992
Jeremy Ho <jujaga@gmail.com>
2021-04-15 16:18:58 -0700

    Routine backend npm package updates Update OpenAPI spec to 3.0.3
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/routes/v1.js
+++ app/src/routes/v1.js
@@ -9,7 +9,7 @@ const helloRouter = require('./v1/hello');
 
 const getSpec = () => {
   const rawSpec = fs.readFileSync(path.join(__dirname, '../docs/v1.api-spec.yaml'), 'utf8');
-  const spec = yaml.safeLoad(rawSpec);
+  const spec = yaml.load(rawSpec);
   spec.servers[0].url = `${config.get('server.basePath')}/api/v1`;
   spec.components.securitySchemes.OpenID.openIdConnectUrl = `${config.get('server.keycloak.serverUrl')}/realms/${config.get('server.keycloak.realm')}/.well-known/openid-configuration`;
   return spec;
@@ -33,7 +33,7 @@ router.get('/docs', (_req, res) => {
 
 /** OpenAPI YAML Spec */
 router.get('/api-spec.yaml', (_req, res) => {
-  res.status(200).type('application/yaml').send(yaml.safeDump(getSpec()));
+  res.status(200).type('application/yaml').send(yaml.dump(getSpec()));
 });
 
 /** OpenAPI JSON Spec */
