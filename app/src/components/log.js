72ac66f57a92b79b9b45463940fd15175c83a847
Jeremy Ho <jujaga@gmail.com>
2021-06-09 16:42:36 -0700

    Log authorized party information from JWT if it exists
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/components/log.js
+++ app/src/components/log.js
@@ -1,8 +1,10 @@
 const config = require('config');
+const jwt = require('jsonwebtoken');
 const { createLogger, format, transports } = require('winston');
 const { logger } = require('express-winston');
 
 const log = createLogger({
+  // defaultMeta: { service: 'scaffold' }, // Add default annotation attribute to log output
   exitOnError: false,
   format: format.combine(
     format.timestamp(), // Add ISO timestamp to each entry
@@ -19,7 +21,6 @@ if (process.env.NODE_ENV !== 'test') {
 
 if (config.has('server.logFile')) {
   log.add(new transports.File({
-    defaultMeta: { service: 'scaffold' },
     filename: config.get('server.logFile'),
     handleExceptions: true
   }));
@@ -27,17 +28,21 @@ if (config.has('server.logFile')) {
 
 const httpLogger = logger({
   colorize: false,
-  dynamicMeta: (req, res) => ({
-    contentLength: res.get('content-length'),
-    httpVersion: req.httpVersion,
-    ip: req.ip,
-    method: req.method,
-    path: req.path,
-    query: Object.keys(req.query).length ? req.query : undefined,
-    responseTime: res.responseTime,
-    statusCode: res.statusCode,
-    userAgent: req.get('user-agent')
-  }),
+  dynamicMeta: (req, res) => {
+    const token = jwt.decode((req.get('authorization') || '').slice(7));
+    return {
+      azp: token && token.azp || undefined,
+      contentLength: res.get('content-length'),
+      httpVersion: req.httpVersion,
+      ip: req.ip,
+      method: req.method,
+      path: req.path,
+      query: Object.keys(req.query).length ? req.query : undefined,
+      responseTime: res.responseTime,
+      statusCode: res.statusCode,
+      userAgent: req.get('user-agent')
+    };
+  },
   expressFormat: true,
   level: 'http',
   meta: true,
@@ -45,7 +50,7 @@ const httpLogger = logger({
   requestWhitelist: [], // Suppress default value output
   responseWhitelist: [], // Suppress default value output
   // Skip logging kube-probe requests
-  skip: (req) => req.headers['user-agent'] && req.headers['user-agent'].includes('kube-probe'),
+  skip: (req) => req.get('user-agent') && req.get('user-agent').includes('kube-probe'),
   winstonInstance: log,
 });
 
